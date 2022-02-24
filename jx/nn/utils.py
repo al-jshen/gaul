6feb824dce277339e54d07e1f42e9d1497e9532d
Jeff Shen <jshen2014@hotmail.com>
2022-02-23 20:31:43 -0500

    write a bunch of nn utils

---
+++ jx/nn/utils.py
@@ -0,0 +1,71 @@
+from typing import Any, Callable, Iterable
+import jax
+import jax.numpy as jnp
+from functools import partial
+
+
+class Sequential:
+    def __init__(self, layers: Iterable[Callable[..., Any]]):
+        self.layers = layers
+
+    @partial(jax.jit, static_argnums=(0,))
+    def __call__(self, inputs, *args, **kwargs):
+        out = inputs
+        for layer in self.layers:
+            out = layer(out, *args, **kwargs)
+        return out
+
+
+class Linear:
+    def __init__(self, input_size: int, output_size: int, key=None):
+        if key is None:
+            key = jax.random.PRNGKey(0)
+        self.weights = jax.random.normal(key, (input_size, output_size))
+        self.bias = jnp.zeros(output_size)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def __call__(self, x):
+        return x @ self.weights + self.bias
+
+
+class Dense(Linear):
+    def __init__(self, input_size: int, output_size: int, activation=None, key=None):
+        super().__init__(input_size, output_size, key)
+        self.input_size = input_size
+        self.output_size = output_size
+        self.activation = activation or (lambda x: x)
+
+    @partial(jax.jit, static_argnums=(0,))
+    def __call__(self, x):
+        return self.activation(super().__call__(x))
+
+
+class Dropout:
+    def __init__(self, p: float, key=None):
+        self.p = jnp.clip(p, 0.0, 1.0)
+        if key is None:
+            self.key = jax.random.PRNGKey(0)
+        else:
+            self.key = key
+
+    @partial(jax.jit, static_argnums=(0,))
+    def __call__(self, x):
+        if self.p == 0.0:
+            return x
+        self.key, subkey = jax.random.split(self.key)
+        keep_rate = 1.0 - self.p
+        keep = jax.random.bernoulli(subkey, keep_rate, shape=x.shape)
+        return keep * x / keep_rate
+
+
+class LayerNorm:
+    def __init__(self, features, eps=1e-3):
+        super().__init__()
+        self.gamma = jnp.ones(features)
+        self.beta = jnp.zeros(features)
+        self.eps = eps
+
+    def __call__(self, x):
+        mean = jnp.mean(x, axis=-1, keepdims=True)
+        std = jnp.std(x, axis=-1, keepdims=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
