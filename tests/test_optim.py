df2b17e5bd4f12435c4e61472edef51877b5c67e
Jeff Shen <jshen2014@hotmail.com>
2022-02-25 00:59:52 -0500

    add pre-commit hooks

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -1,11 +1,13 @@
-import jax
 import jax.numpy as jnp
+
 import jacket.optim.optimizers as optimizers
 
 
 class TestSGD:
     def test_1d(self):
-        loss = lambda x: jnp.sum(x**2)
+        def loss(x):
+            jnp.sum(x**2)
+
         x = jnp.array([1.0])
         estimate = optimizers.sgd(loss, x, 0.01, 1000)
         solution = 0.0
@@ -14,7 +16,10 @@ class TestSGD:
     def test_rosenbrock(self):
         a = 1.0
         b = 10.0
-        loss = lambda x: (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
+
+        def loss(x):
+            (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
+
         x = jnp.zeros(2)
         estimate = optimizers.momentum(loss, x, 0.01, 5000, 0.9, True)
         solution = jnp.array([a, a**2])
