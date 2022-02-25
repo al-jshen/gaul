be46b1f7a63524bacf7af40d8634a74a23fb901a
Jeff Shen <jshen2014@hotmail.com>
2022-02-25 00:44:29 -0500

    write SGD with nesterov momentum; add test

--- jacket/optim/optimizers.py
+++ jacket/optim/optimizers.py
@@ -1,8 +1,9 @@
 from typing import Callable
+from functools import partial
 
 import jax
 
-from jacket.utils import Pytree
+from jacket.utils import Pytree, tree_zeros_like
 
 
 def sgd(
@@ -15,5 +16,37 @@ def sgd(
 
     for _ in range(niter):
         params = update(params, *args, **kwargs)
+
+    return params
+
+
+def momentum(
+    fn: Callable,
+    params: Pytree,
+    lr: float = 1e-3,
+    niter: int = 500,
+    momentum: float = 0.9,
+    nesterov: bool = False,
+    *args,
+    **kwargs,
+) -> Pytree:
+    @partial(jax.jit, static_argnames=["nesterov"])
+    def update(params, velocity, nesterov=False, *args, **kwargs):
+        if nesterov:
+            alt_params = jax.tree_util.tree_multimap(
+                lambda p, v: p - momentum * v, params, velocity
+            )
+            grads = jax.grad(fn)(alt_params, *args, **kwargs)
+        else:
+            grads = jax.grad(fn)(params, *args, **kwargs)
+
+        velocity = jax.tree_util.tree_multimap(
+            lambda v, g: momentum * v + lr * g, velocity, grads
+        )
+        return jax.tree_util.tree_multimap(lambda p, v: p - v, params, velocity)
+
+    velocity = tree_zeros_like(params)
+    for _ in range(niter):
+        params = update(params, velocity, nesterov=nesterov, *args, **kwargs)
 
     return params
