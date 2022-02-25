1284f37ab776ebe8584f0f50c55c80d55e2084f5
Jeff Shen <jshen2014@hotmail.com>
2022-02-25 01:09:15 -0500

    ignore/revert E731

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -5,9 +5,7 @@ import jacket.optim.optimizers as optimizers
 
 class TestSGD:
     def test_1d(self):
-        def loss(x):
-            jnp.sum(x**2)
-
+        loss = lambda x: jnp.sum(x**2)
         x = jnp.array([1.0])
         estimate = optimizers.sgd(loss, x, 0.01, 1000)
         solution = 0.0
@@ -16,10 +14,7 @@ class TestSGD:
     def test_rosenbrock(self):
         a = 1.0
         b = 10.0
-
-        def loss(x):
-            (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
-
+        loss = lambda x: (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
         x = jnp.zeros(2)
         estimate = optimizers.momentum(loss, x, 0.01, 5000, 0.9, True)
         solution = jnp.array([a, a**2])
