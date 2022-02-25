79a7fb47ce5a788105cfb45694b85584c7c0d531
Jeff Shen <jshen2014@hotmail.com>
2022-02-24 22:22:03 -0500

    rewrite hmc to work on trees; add type annotations to hmc

--- jacket/modelling/hmc.py
+++ jacket/modelling/hmc.py
@@ -1,55 +1,91 @@
+from typing import Callable, Optional, Tuple
 import jax
-from jax._src.flatten_util import ravel_pytree
 import jax.numpy as jnp
 from tqdm import tqdm
+from jacket.utils import tree_random_normal_like, tree_stack, Pytree
 
 
-def factor(params, momentum, ln_posterior, inv_mass, *args, **kwargs):
-    return (
-        ln_posterior(params, *args, **kwargs) - 0.5 * momentum.T @ inv_mass @ momentum
-    )
+def factor(
+    params: Pytree,
+    momentum: Pytree,
+    ln_posterior: Callable[..., float],
+    *args,
+    **kwargs,
+) -> float:
+    m = jax.tree_util.tree_reduce(lambda acc, x: acc + x.T @ x, momentum, 0.0)
+    return ln_posterior(params, *args, **kwargs) - 0.5 * m
 
 
-def accept_reject(state_old, state_new, ln_posterior, inv_mass, key, *args, **kwargs):
+def accept_reject(
+    state_old: Tuple[Pytree, Pytree],
+    state_new: Tuple[Pytree, Pytree],
+    ln_posterior: Callable[..., float],
+    key,
+    *args,
+    **kwargs,
+) -> Tuple[Pytree, Pytree, float, bool]:
     params_old, momentum_old = state_old
     params_new, momentum_new = state_new
 
-    factor_old = factor(
-        params_old, momentum_old, ln_posterior, inv_mass, *args, **kwargs
-    )
-    factor_new = factor(
-        params_new, momentum_new, ln_posterior, inv_mass, *args, **kwargs
-    )
+    factor_old = factor(params_old, momentum_old, ln_posterior, *args, **kwargs)
+    factor_new = factor(params_new, momentum_new, ln_posterior, *args, **kwargs)
     p_accept = jnp.clip(jnp.exp(factor_new - factor_old), a_max=1)
 
     if jax.random.uniform(key) < p_accept:
-        return params_new, -momentum_new, p_accept, 1
+        neg_momentum_new = jax.tree_util.tree_map(lambda x: -x, momentum_new)
+        return params_new, neg_momentum_new, p_accept, True
     else:
-        return params_old, momentum_old, p_accept, 0
+        return params_old, momentum_old, p_accept, False
+
+
+def leapfrog_step(
+    params: Pytree,
+    momentum: Pytree,
+    step_size: float,
+    grad_fn: Callable,
+    *args,
+    **kwargs,
+) -> Tuple[Pytree, Pytree]:
+    momentum = jax.tree_util.tree_multimap(
+        lambda m, g: m + g * 0.5 * step_size, momentum, grad_fn(params, *args, **kwargs)
+    )
+    params = jax.tree_util.tree_multimap(
+        lambda p, m: p + m * step_size, params, momentum
+    )
+    momentum = jax.tree_util.tree_multimap(
+        lambda m, g: m + g * 0.5 * step_size, momentum, grad_fn(params, *args, **kwargs)
+    )
+    return params, momentum
 
 
 def leapfrog(
-    params_init, momentum_init, n_steps, stepsize, grad_fn, inv_mass, *args, **kwargs
-):
+    params_init: Pytree,
+    momentum_init: Pytree,
+    n_steps: int,
+    step_size: float,
+    grad_fn: Callable,
+    *args,
+    **kwargs,
+) -> Tuple[Pytree, Pytree]:
     params, momentum = params_init, momentum_init
     for _ in range(n_steps):
-        momentum = momentum + grad_fn(params, *args, **kwargs) * 0.5 * stepsize
-        params = params + inv_mass @ momentum * stepsize
-        momentum = momentum + grad_fn(params, *args, **kwargs) * 0.5 * stepsize
+        params, momentum = leapfrog_step(
+            params, momentum, step_size, grad_fn, *args, **kwargs
+        )
     return params, momentum
 
 
 def sample(
-    ln_posterior,
-    init_params,
-    leapfrog_steps=10,
-    step_size=1e-3,
-    n_samples=1000,
-    n_warmup=1000,
+    ln_posterior: Callable[..., float],
+    init_params: Pytree,
+    leapfrog_steps: int = 10,
+    step_size: float = 1e-3,
+    n_samples: int = 1000,
+    n_warmup: int = 1000,
     key=None,
     *args,
     **kwargs,
-):
+) -> Tuple[Pytree, Pytree]:
     if key is None:
         key = jax.random.PRNGKey(0)
 
@@ -58,19 +94,12 @@ def sample(
 
     ln_posterior_grad = jax.jit(jax.grad(ln_posterior))
 
-    params, ravel_fn = ravel_pytree(init_params)
-    ravel_vmap = jax.vmap(ravel_fn)
-    n_params = params.size
-
-    mass = jnp.eye(n_params, n_params)
-    inv_mass = jnp.eye(n_params, n_params)
+    params = init_params
 
     for i in tqdm(range(n_samples + n_warmup)):
 
         key, subkey = jax.random.split(key)
-        momentum = jax.random.multivariate_normal(
-            subkey, mean=jnp.zeros(n_params), cov=mass
-        )
+        momentum = tree_random_normal_like(subkey, params)
 
         params_new, momentum_new = leapfrog(
             params,
@@ -78,7 +107,6 @@ def sample(
             leapfrog_steps,
             step_size,
             ln_posterior_grad,
-            inv_mass,
             *args,
             **kwargs,
         )
@@ -88,7 +116,6 @@ def sample(
             (params, momentum),
             (params_new, momentum_new),
             ln_posterior,
-            inv_mass,
             subkey,
         )
 
@@ -96,4 +123,4 @@ def sample(
             samples.append(params)
             samples_momentum.append(momentum)
 
-    return ravel_vmap(jnp.array(samples)), ravel_vmap(jnp.array(samples_momentum))
+    return tree_stack(samples), tree_stack(samples_momentum)
